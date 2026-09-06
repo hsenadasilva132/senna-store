cedadb16ccdd38555483c612e6206adec68716e0
Henrique Sena <135704597+hsenadasilva132@users.noreply.github.com>
2026-09-05 23:45:40 -0300

    fix: configure CORS for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,12 +43,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
+origins = [
+    "http://localhost:5173",
+    "https://senna-store.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
